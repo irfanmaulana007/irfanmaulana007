ea81dc4f2ce6d98bbaf3d18a9d6108b447b73c9d
Irfan Maulana <irfan.maulana@evos.gg>
2023-10-09 20:04:31 +0700

    test

--- apps/ads-sandbox/app/page.tsx
+++ apps/ads-sandbox/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <main className='container mx-auto'>
-            <div className='min-h-screen flex items-center justify-center bg-gray-700'>
+            <div className='min-h-screen flex items-center justify-center bg-gray-800'>
                 <h3>Ads Sandbox</h3>
             </div>
         </main>
